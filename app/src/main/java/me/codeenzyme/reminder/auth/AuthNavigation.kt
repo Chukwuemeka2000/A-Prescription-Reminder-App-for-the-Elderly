2743863e4a19a22a13bc882d3590364425e084ca
Victor-El <victorelezua@gmail.com>
2022-02-28 07:22:07 +0100

    add comments

--- app/src/main/java/me/codeenzyme/reminder/auth/AuthNavigation.kt
+++ app/src/main/java/me/codeenzyme/reminder/auth/AuthNavigation.kt
@@ -1,5 +1,6 @@
 package me.codeenzyme.reminder.auth
 
+// Used for managing navigation routes
 sealed class AuthScreen(val route: String) {
     object Login: AuthScreen("login")
     object SignUp: AuthScreen("signup")
--- app/src/main/java/me/codeenzyme/reminder/auth/AuthViewModel.kt
+++ app/src/main/java/me/codeenzyme/reminder/auth/AuthViewModel.kt
@@ -6,6 +6,7 @@ class AuthViewModel : ViewModel() {
 
     private val repo = AuthRepository()
 
+    // calls the repository to handle sign up process
     fun signUp(
         firstName: String,
         lastName: String,
@@ -17,6 +18,7 @@ class AuthViewModel : ViewModel() {
         repo.signUp(firstName, lastName, email, password, phone, onCompletion)
     }
 
+    // call the auth repository to handle login process
     fun login(email: String, password: String, onCompletion: (AuthStatus) -> Unit) {
         repo.login(email, password, onCompletion)
     }
