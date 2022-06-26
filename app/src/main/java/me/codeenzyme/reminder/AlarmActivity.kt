69a2504810e582081cccffd3f4319974ac8eb630
Victor Elezua <victorelezua@gmail.com>
2022-06-26 20:37:22 +0100

    add alarm activity and default tone

--- app/src/main/java/me/codeenzyme/reminder/AlarmActivity.kt
+++ app/src/main/java/me/codeenzyme/reminder/AlarmActivity.kt
@@ -1,10 +1,14 @@
 package me.codeenzyme.reminder
 
+import android.media.AudioFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.nisrulz.zentone.ZenTone
 import me.codeenzyme.reminder.databinding.ActivityAlarmBinding
 
 class AlarmActivity : AppCompatActivity() {
+
+    val zenTone = ZenTone(channelMask = AudioFormat.CHANNEL_OUT_STEREO)
 
     private var title: String? = null
     private var message: String? = null
@@ -13,6 +17,9 @@ class AlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        zenTone.play(440F, 100)
+
         viewBinding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(viewBinding?.root)
 
@@ -28,5 +35,7 @@ class AlarmActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         viewBinding = null
+        zenTone.stop()
+        zenTone.release()
     }
 }
