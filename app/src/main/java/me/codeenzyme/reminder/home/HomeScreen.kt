d6dbc210c4936f15850dd3bc7cf94125b7f1f52e
Victor <victorelezua@gmail.com>
2022-12-16 08:38:35 +0100

    add grid

--- app/src/main/java/me/codeenzyme/reminder/home/HomeScreen.kt
+++ app/src/main/java/me/codeenzyme/reminder/home/HomeScreen.kt
@@ -8,6 +8,9 @@ import android.widget.Toast
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -19,12 +22,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -33,6 +41,7 @@ import com.google.firebase.Timestamp
 import me.codeenzyme.reminder.MedicationRepoStatus
 import me.codeenzyme.reminder.R
 import me.codeenzyme.reminder.Util
+import me.codeenzyme.reminder.ui.theme.ColorWhite
 import java.lang.StrictMath.abs
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
@@ -161,7 +170,7 @@ fun HomeScreen() {
         }
     }
 
-    Scaffold(modifier = Modifier.padding(bottom = 50.dp), floatingActionButton = {
+    Scaffold(modifier = Modifier.padding(bottom = 50.dp), backgroundColor = Color(0xFFDDDDDD), floatingActionButton = {
         FloatingActionButton(onClick = { showAddDialog = true },) {
             Icon(imageVector = Icons.Default.Add, contentDescription = "Add new medication",)
         }
@@ -177,9 +186,15 @@ fun HomeScreen() {
 
             Column(modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = "Number of Medicines", color = MaterialTheme.colors.primaryVariant, fontSize = 26.sp)
-                Text(text = "${data?.size ?: 0}", fontSize = 24.sp)
+                .padding(0.dp)
+                .background(
+                    MaterialTheme.colors.primaryVariant,
+                    RoundedCornerShape(bottomEnd = 48.dp, bottomStart = 48.dp)
+                ), horizontalAlignment = Alignment.CenterHorizontally) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Number of Medicines", color = ColorWhite, fontSize = 26.sp)
+                Text(text = "${data?.size ?: 0}", fontSize = 24.sp, color = ColorWhite)
+                Spacer(modifier = Modifier.height(16.dp))
             }
 
             data?.let {
@@ -189,13 +204,15 @@ fun HomeScreen() {
                     }
                 } else {
                     it.forEachIndexed { index, model ->
-
+                        Log.d("ELEZUA", model.toString())
                         medicationViewModel.setAlarm(model.medicationName!!, model.medicationDescription!!, model.medicationDosage!!, model.medicationDosageType!!, model.alarmId!!, (model.startTime!!.toDate().time), model.medicationInterval!!.toLong())
                         Log.e("time stamp", model.startTime!!.toDate().toString())
                     }
-                    LazyColumn(modifier = Modifier
+                    LazyVerticalGrid(modifier = Modifier
                         .fillMaxSize()
-                        .padding(16.dp)) {
+                        .padding(16.dp),
+                        columns = GridCells.Fixed(2)
+                    ) {
                         items(it) { medication ->
                             MedicationItem(medication, ::deleteMedication)
                         }
@@ -365,16 +382,44 @@ fun MedicationItem(medicationModel: MedicationModel, delete: (medicationModel: M
     Card(modifier = Modifier
         .padding(vertical = 8.dp)
         .fillMaxWidth()
-        .padding(8.dp), backgroundColor = Color.LightGray) {
+        .padding(8.dp), backgroundColor = Color(0xFFFFFFFF)) {
         Box(modifier = Modifier.fillMaxWidth()) {
             IconButton(onClick = { delete(medicationModel) }, modifier = Modifier.align(Alignment.TopEnd)) {
                 Icon(imageVector = Icons.Default.Delete, contentDescription = "")
             }
             Column(modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)) {
-                Text(medicationModel.medicationName!!)
-                Text(medicationModel.medicationDescription!!)
+                .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally,) {
+                when (medicationModel.medicationDosageType?.toUpperCase()) {
+                    "PILL" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_pill), contentDescription = "pill", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    "TABLET" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_pill), contentDescription = "tablet", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    "CAPSULE" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_capsule), contentDescription = "capsule", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    "INJECTION" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_syringe), contentDescription = "injection", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    "TABLESPOON" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_syrup), contentDescription = "tablespoon", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    "TEASPOON" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_syrup), contentDescription = "teaspoon", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    "DROPS" -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_syrup), contentDescription = "syrup", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                    else -> {
+                        Icon(painter = painterResource(id = R.drawable.ic_pill), contentDescription = "pill", tint = MaterialTheme.colors.primaryVariant, modifier = Modifier.size(48.dp))
+                    }
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(medicationModel.medicationName!!, overflow = TextOverflow.Ellipsis, fontSize = 20.sp, fontWeight = FontWeight.Bold, maxLines = 1)
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(medicationModel.medicationDescription!!, overflow = TextOverflow.Ellipsis, maxLines = 2)
                 Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
                     Text(medicationModel.medicationInterval.toString())
                     Text("${medicationModel.medicationDosage} ${medicationModel.medicationDosageType}")
